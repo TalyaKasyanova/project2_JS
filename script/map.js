47d6ff40c888ec9798694a16acd0c119ac3b6389
TalyaKasyanova <talyakasyanova7@gmail.com>
2023-03-29 22:04:11 +0300

    map

--- script/map.js
+++ script/map.js
@@ -1,152 +1,51 @@
-function initMap() {}
-
-initMap = function () {
-  let myLatLng = {
-    lat: 46.64798304,
-    lng: 32.63328588
+function initializeMap() {
+  // координати міста Monticello в США
+  const monticelloCoords = {
+    lat: 42.2242,
+    lng: -74.7853
   };
-
-  let map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 16,
-    center: myLatLng,
-    disableDefaultUI: true,
-    mapTypeControl: true,
-    styles: [{
-        elementType: "geometry",
-        stylers: [{
-          color: "#f5f5f5"
-        }]
-      },
-      {
-        elementType: "labels.icon",
-        stylers: [{
-          visibility: "off"
-        }]
-      },
-      {
-        elementType: "labels.text.fill",
-        stylers: [{
-          color: "#616161"
-        }]
-      },
-      {
-        elementType: "labels.text.stroke",
-        stylers: [{
-          color: "#f5f5f5"
-        }]
-      },
-      {
-        featureType: "administrative.land_parcel",
-        elementType: "labels.text.fill",
-        stylers: [{
-          color: "#bdbdbd"
-        }]
-      },
-      {
-        featureType: "poi",
-        elementType: "geometry",
-        stylers: [{
-          color: "#eeeeee"
-        }]
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [{
-          color: "#757575"
-        }]
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [{
-          color: "#e5e5e5"
-        }]
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [{
-          color: "#9e9e9e"
-        }]
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [{
-          color: "#ffffff"
-        }]
-      },
-      {
-        featureType: "road.arterial",
-        elementType: "labels.text.fill",
-        stylers: [{
-          color: "#757575"
-        }]
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [{
-          color: "#dadada"
-        }]
-      },
-      {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [{
-          color: "#616161"
-        }]
-      },
-      {
-        featureType: "road.local",
-        elementType: "labels.text.fill",
-        stylers: [{
-          color: "#9e9e9e"
-        }]
-      },
-      {
-        featureType: "transit.line",
-        elementType: "geometry",
-        stylers: [{
-          color: "#e5e5e5"
-        }]
-      },
-      {
-        featureType: "transit.station",
-        elementType: "geometry",
-        stylers: [{
-          color: "#eeeeee"
-        }]
-      },
-      {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [{
-          color: "#c9c9c9"
-        }]
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [{
-          color: "#9e9e9e"
-        }]
-      }
-    ]
+  // створення карти і центрування на місті Monticello
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: monticelloCoords,
   });
 
-  let image = new google.maps.MarkerImage(
-    "../images/Pin.png",
-    new google.maps.Size(130, 130),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(22, 32)
-  );
+  // додавання відмітки на центр міста
+  const infowindow = new google.maps.InfoWindow({
+    content: '<h3>Центр міста Monticello</h3>'
+  });
 
-  let marker = new google.maps.Marker({
-    position: myLatLng,
-    icon: image,
+  const marker = new google.maps.Marker({
+    position: monticelloCoords,
     map: map,
-    title: "Monticello"
+    title: 'Центр міста Monticello'
   });
-};
+
+  // відкривання відмітки при завантаженні сторінки
+  infowindow.open(map, marker);
+}
+
+window.initializeMap = initializeMap;
+
+
+// function initMap() {
+//   const mapFrame = document.getElementById("map");
+//   const location = mapFrame.src.split("q=")[1].split("&")[0].replace(/\+/g, " ");
+//   const geocoder = new google.maps.Geocoder();
+//   geocoder.geocode({ address: location }, function (results, status) {
+//     if (status === "OK") {
+//       const map = new google.maps.Map(mapFrame, {
+//         zoom: 14,
+//         center: results[0].geometry.location,
+//       });
+//       const marker = new google.maps.Marker({
+//         position: results[0].geometry.location,
+//         map: map,
+//       });
+//     } else {
+//       console.error("Geocode was not successful for the following reason: " + status);
+//     }
+//   });
+// }
+
+// window.initMap = initMap;
